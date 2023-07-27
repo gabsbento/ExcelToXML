a60836f0930d9acaab7cdb3468b4756d1e6a0d7d
Gabriel Bento <gab.silva.bento@gmail.com>
2023-07-27 13:33:21 -0300

    testando classe de leitura de excel

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import org.example.controller.ExcelToXMLConverter;
+
+import java.io.File;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        ExcelToXMLConverter arquivo = new ExcelToXMLConverter(new File(".\\src\\main\\java\\org\\example\\teste.xlsx"));
+        System.out.println("teste: "+arquivo.getSheet().getRow(0));
 
     }
 }
--- src/main/java/org/example/controller/ExcelToXMLConverter.java
+++ src/main/java/org/example/controller/ExcelToXMLConverter.java
@@ -5,16 +5,19 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.example.template.ContratoExcel;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 public class ExcelToXMLConverter {
-    Workbook workbook;
-    Sheet sheet;
+    private Workbook workbook;
+    private Sheet sheet;
     public ExcelToXMLConverter(File inputFile){
         try{
-            workbook = new XSSFWorkbook(inputFile);
-            sheet = workbook.getSheetAt(0);
+            this.workbook = new XSSFWorkbook(inputFile);
+            this.sheet = workbook.getSheetAt(0);
         } catch (Exception e ){
             e.printStackTrace();
         }
@@ -26,7 +29,25 @@ public class ExcelToXMLConverter {
     }
 
     public List<ContratoExcel> readSheetData(Sheet sheet){
+        System.out.println(sheet.getActiveCell());
         return null;
     }
+
+    public boolean createXML(File inputFile){
+        try{
+            JAXBContext context = JAXBContext.newInstance(ContratoExcel.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(readSheetData(getSheet()), new FileOutputStream(inputFile));
+
+            System.out.println("Arquivo xml criado");
+            return true;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
 
 }
